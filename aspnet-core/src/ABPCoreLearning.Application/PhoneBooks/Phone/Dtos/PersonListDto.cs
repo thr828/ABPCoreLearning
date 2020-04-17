ee42fb7bfbba78f2db62b17cdc1c72bf0d5c09ee
thr828 <592040808@qq.com>
2020-04-18 00:01:34 +0800

    应用层增加逻辑代码

---
+++ aspnet-core/src/ABPCoreLearning.Application/PhoneBooks/Phone/Dtos/PersonListDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abp.Application.Services.Dto;
+using ABPCoreLearning.PhoneBooks.PhoneNumbers;
+
+namespace ABPCoreLearning.PhoneBooks.Phone.Dtos
+{
+    public class PersonListDto:FullAuditedEntityDto
+    {
+        public string Name { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Address { get; set; }
+
+        public List<PhoneNumber> PhoneNumbers { get; set; }
+    }
+}
---
+++ aspnet-core/src/ABPCoreLearning.Application/PhoneBooks/Phone/IPersonAppService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abp.Application.Services;
+
+namespace ABPCoreLearning.PhoneBooks.Phone
+{
+    public interface IPersonAppService:IApplicationService
+    {
+
+    }
+}
